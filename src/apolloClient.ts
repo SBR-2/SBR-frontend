ae9087d9356326217f185e6ccd64a50d26354772
ErickTejada04 <erick1310lol@gmail.com>
2024-10-19 19:35:36 -0400

    fix: conflict

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,48 +1,28 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client/core';
-import { setContext } from '@apollo/client/link/context';
-import { createHttpLink } from '@apollo/client/link/http';
-import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
-
+import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import { createHttpLink } from "@apollo/client/link/http";
+import { setContext } from "@apollo/client/link/context";
 
 // Obtener variable de entorno o endpoint default
-const apiUrl = import.meta.env.VITE_GRAPHQL_API_URL || 'http://localhost:5274/graphql';
+const apiUrl =
+  import.meta.env.VITE_GRAPHQL_API_URL || "http://localhost:5274/graphql/";
 
-const uploadLink = createUploadLink({
-    uri: apiUrl,
-    headers: {
-      'GraphQL-preflight': '1'
-    }
-  })
-  
-  // Mantener la configuración de autenticación
-  const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('token')
+const httpLink = createHttpLink({
+  uri: apiUrl,
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('accessToken');
     return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : '',
-        'GraphQL-preflight': '1'
-      }
-    }
-  })
-  
-  // Crear el cliente Apollo con la nueva configuración
-  const apolloClient = new ApolloClient({
-    link: authLink.concat(uploadLink),
-    cache: new InMemoryCache(),
-    defaultOptions: {
-      mutate: {
-        errorPolicy: 'all'
-      },
-      watchQuery: {
-        fetchPolicy: 'no-cache',
-        errorPolicy: 'all'
-      },
-      query: {
-        fetchPolicy: 'no-cache',
-        errorPolicy: 'all'
-      }
-    }
-  })
-  
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const apolloClient = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 export default apolloClient;
